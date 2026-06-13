c91a57809818af326be1b7a34fa3f86fce03cd20
Tiago Beraldo <tiagoberaldodelima@gmail.com>
2026-06-13 12:42:58 -0300

    Adição dos comentários, versão final

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+// seleciona todos os slides e botões de ponto
 const slides = document.querySelectorAll('.slide');
-
 const pontos = document.querySelectorAll('.dot');
-
 let slideAtual = 0;
 
+// inicializa o carrossel no primeiro slide
 irParaSlide(0);
 
+// remove a classe ativo de todos os slides e adiciona apenas no escolhido
 function irParaSlide(numero) {
     slides.forEach(function(slide) {
         slide.classList.remove('ativo');
 });
-
 slides[numero].classList.add('ativo');
-
 slideAtual = numero;
 }
